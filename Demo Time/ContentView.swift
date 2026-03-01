465157bead2135dfa49d003fcc8b14eda01dbc03
yavik-kapadia <yavikkapadia@gmail.com>
2026-03-01 13:34:34 -0800

    Initial Commit

---
+++ Demo Time/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo Time
+//
+//  Created by Yavik on 3/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo Time/Demo_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_TimeApp.swift
+//  Demo Time
+//
+//  Created by Yavik on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Demo_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
